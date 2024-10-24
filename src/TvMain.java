7e6fc4c29c908e2f4a53d7233b89e0f992aa68c7
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2024-10-24 10:37:04 +0200

    Commitus

--- src/TvMain.java
+++ src/TvMain.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class TvMain {
+
+    private ArrayList<TvSeries> shows;
+
+    public TvMain() {
+    }
+
+    public void Write2File(){
+
+    }
+
+    public void readFromFile(){}
+
+    public void createData(){}
+
+
+
 }
--- src/TvSeries.java
+++ src/TvSeries.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.Serializable;
 
-public class TvSeries {
+public class TvSeries implements Serializable {
+    Scanner scanner = new Scanner(System.in);
+    private String name;
+    private ArrayList<Integer> episodes = new ArrayList<>();
+    private int rating;
+
+    public TvSeries() {
+        System.out.println("Tv-Shows name?");
+        name = scanner.nextLine();
+    }
+
+    public TvSeries(String inName) {
+        this.name = inName;
+    }
+
+
+    public void addEpisodes(int numOfEpisodes, int season) {
+
+        if (season > episodes.size()) {
+            for (int i = 0; i < season - 1; i++) {
+                episodes.add(0);
+            }
+        }
+        episodes.add(season - 1, numOfEpisodes);
+
+    }
+
+    public void changeRating(int newRating) {
+        rating = newRating;
+    }
+
+    public void printInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Rating: " + rating);
+    }
+
 }
